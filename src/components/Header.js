581c7e02f3646a27c566735fe8793d730753f1b1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-09 14:53:55 -0500

    [+] Header: Estructura y Estilos

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Header = ({ title }) => (     // Return Implicito & Destructuracion del Props
     <nav>
-    <div className="nav-wrapper">
+    <div className="nav-wrapper light-blue darken-2">
         <a href="#" className="brand-logo">{ title }</a>
     </div>
+    <div className="form-container">
+        <div className="container">
+            <div className="row">
+                <div className="col m6 s12">Formulario</div>
+                <div className="col m6 s12">Datos del clima</div>
+            </div>
+        </div>
+    </div>
+
   </nav>
 );
 
