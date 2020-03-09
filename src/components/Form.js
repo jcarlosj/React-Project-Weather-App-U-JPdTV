4ce9c02855b1c8aa4bf8def9671e16395d78c742
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-09 15:51:52 -0500

    [+] Form > State/handleChange: Ciudad, Pais

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+    /** Define State */
+    const [ data, setData ] = useState({
+        city: '',
+        country: ''
+    });
+
+    const { city, country } = data;     // Destructuring State 'data'
+
+    /** Agrega datos del formulario en el State */
+    const handleChange = event => {
+        setData({
+            ...data,
+            [ event .target .name ]: event .target .value       /** Valores de los campos del Formulario */
+        });
+    }
+
     return (
         <form className="col s12">
             <div className="row">
                 <div className="input-field col s12">
-                    <input id="city" type="text" className="validate" />
+                    <input 
+                        id="city" 
+                        name="city"
+                        type="text" 
+                        className="validate" 
+                        value={ city }
+                        onChange={ handleChange }
+                    />
                     <label htmlFor="city">Ciudad</label>
                 </div>
                 <div className="input-field col s12">
-                    <select id="country" id="country">
+                    <select 
+                        id="country" 
+                        name="country"
+                        value={ country }
+                        onChange={ handleChange }
+                    >
                         <option value="">Seleccione...</option>
                         <option value="US">Estados Unidos</option>
                         <option value="MX">México</option>
