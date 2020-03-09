bc52e7803b555379b8ddad4a85b45e207bb056f8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-09 15:31:41 -0500

    [+] Form > fields: Ciudad, Pais

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <h2>Formulario</h2>
+        <form className="col s12">
+            <div className="row">
+                <div className="input-field col s12">
+                    <input id="city" type="text" className="validate" />
+                    <label htmlFor="city">Ciudad</label>
+                </div>
+                <div className="input-field col s12">
+                    <select id="country" id="country">
+                        <option value="">Seleccione...</option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
+                    </select>
+                    <label htmlFor="country">País</label>
+                </div>
+            </div>
+        </form>
     );
 }
 
